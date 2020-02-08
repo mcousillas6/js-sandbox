242605a8dfb3a46aa7d6dbd7a0499808679b1839
Mauricio Cousillas <cousillas806@gmail.com>
2020-02-08 11:09:25 -0300

    Initial commit

---
+++ tests/test.spec.js
@@ -0,0 +1,8 @@
+const myFunction = require('../index');
+
+describe('Test function behaviour', () => {
+  it('should work', () => {
+    const result = myFunction();
+    expect(result).toEqual(1);
+  });
+})
